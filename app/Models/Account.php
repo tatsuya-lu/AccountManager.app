40edeb25d892f9b0ad4553d3980db92dc0ce68fe
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2024-01-23 13:12:37 +0900

    モデルとマイグレーションファイルの作成

---
+++ app/Models/Account.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Account extends Model implements Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'accounts';
+
+    protected $fillable = [
+        'name',
+        'sub_name',
+        'email',
+        'password',
+        'tel',
+        'post_code',
+        'prefecture',
+        'city',
+        'street',
+        'body',
+        'isAdmin',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'isAdmin' => 'boolean',
+    ];
+
+    public function deleteBookById($id)
+    {
+        return $this->destroy($id);
+    }
+
+    public function getAuthIdentifierName() {
+        return 'id';
+    }
+
+    public function getAuthIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getAuthPassword() {
+        return $this->password;
+    }
+
+    public function getRememberToken() {
+        return $this->remember_token; // ユーザーの"remember_token"を取得します
+    }
+
+    public function setRememberToken($value) {
+        $this->remember_token = $value; // ユーザーの"remember_token"を設定します
+    }
+
+    public function getRememberTokenName() {
+        return 'remember_token'; // "remember_token" カラム名を指定します
+    }
+}
