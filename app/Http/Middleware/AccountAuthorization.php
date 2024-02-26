40517ad2e4e220110c1a0e5b91ae71ae3ba785f6
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2024-02-26 13:54:54 +0900

    確認作業/コメントの削除

--- app/Http/Middleware/AccountAuthorization.php
+++ app/Http/Middleware/AccountAuthorization.php
@@ -8,18 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AccountAuthorization
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        $currentUser = $request->user('admin'); // 現在のログインユーザーを取得します。
+        $currentUser = $request->user('admin'); 
 
-        // 現在のユーザーが存在し、かつ管理者であるかどうかを確認します。
         if ($currentUser !== null && $currentUser->admin_level == 1) {
-            return $next($request); // 管理者の場合は次の処理に進みます。
+            return $next($request); 
         }
 
         abort(403, '権限が無いためこの操作を実行できません。');
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -8,9 +8,6 @@ use Illuminate\Auth\AuthenticationException;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
@@ -33,6 +30,4 @@ class Authenticate extends Middleware
             }
         }
     }
-
-
 }
