86dd51752f040cc286b71dddf0c0e218f9604bbf
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2024-02-26 13:51:48 +0900

    確認作業/コメントの削除

--- app/Http/Requests/Account/AccountRequest.php
+++ app/Http/Requests/Account/AccountRequest.php
@@ -7,21 +7,11 @@ use Illuminate\Validation\Rule;
 
 class AccountRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         $uniqueRule = Rule::unique('accounts')->ignore($this->route('user'));
@@ -46,12 +36,6 @@ class AccountRequest extends FormRequest
         ];
     }
 
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function messages()
     {
         return [
