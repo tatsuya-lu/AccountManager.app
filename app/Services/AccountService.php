1e5af2f7c3ce7071eddd8c208ccc069d3653ab6e
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2024-03-11 14:52:48 +0900

    アカウントの一覧表示メソッドをサービスクラスへ移動

--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -7,6 +7,50 @@ use App\Models\Account;
 
 class AccountService
 {
+
+    public function accountList($request)
+    {
+        $sort = $request->input('sort', 'newest');
+
+        $query = Account::query();
+
+        switch ($sort) {
+            case 'newest':
+                $query->orderBy('created_at', 'desc');
+                break;
+            case 'oldest':
+                $query->orderBy('created_at', 'asc');
+                break;
+            default:
+                $query->orderBy('created_at', 'desc');
+                break;
+        }
+
+        $users = $query->where(function ($query) use ($request) {
+            if ($searchName = $request->input('search_name')) {
+                $query->where('name', 'LIKE', '%' . $searchName . '%');
+            }
+
+            if ($searchAdminLevel = $request->input('search_admin_level')) {
+                $adminLevelValue = is_numeric($searchAdminLevel) ? $searchAdminLevel : ($searchAdminLevel == '社員' ? 1 : ($searchAdminLevel == '管理者' ? 2 : null));
+                if ($adminLevelValue !== null) {
+                    $query->where('admin_level', $adminLevelValue);
+                }
+            }
+
+            if ($searchEmail = $request->input('search_email')) {
+                $query->where('email', 'LIKE', '%' . $searchEmail . '%');
+            }
+        })->paginate(20);
+
+        foreach ($users as $user) {
+            $user->prefecture = config('const.prefecture.' . $user->prefecture);
+            $user->admin_level = $user->admin_level == 1 ? '管理者' : ($user->admin_level == 2 ? '社員' : '');
+        }
+
+        return $users;
+    }
+
     public function register(array $data)
     {
         $user = Account::create([
