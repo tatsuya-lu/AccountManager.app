f9ef17e8b544936cbfac4901a2ed1ed0b824d4dd
DESKTOP-RAMBKI6\USER <tatuya16228@gmail.com>
2024-03-11 15:58:11 +0900

    お問い合わせ一覧・検索メソッドをサービスクラスへ移動

--- app/Http/Controllers/Account/InquiryController.php
+++ app/Http/Controllers/Account/InquiryController.php
@@ -3,71 +3,27 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Post;
-use App\Http\Requests\Account\InquiryRequest;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Http\Request;
+use App\Http\Requests\Account\InquiryRequest;
+use App\Models\Post;
+use App\Services\InquiryService;
 
 class InquiryController extends Controller
 {
+
+    protected $inquiryService;
+
+    public function __construct(InquiryService $inquiryService)
+    {
+        $this->inquiryService = $inquiryService; 
+    }
+
     public function index(Request $request)
     {
-        $sort = $request->input('sort', 'newest');
+        $inquiries = $this->inquiryService->index($request);
 
-        $query = Post::query();
-
-        switch ($sort) {
-            case 'newest':
-                $query->orderBy('created_at', 'desc');
-                break;
-            case 'oldest':
-                $query->orderBy('created_at', 'asc');
-                break;
-            default:
-                $query->orderBy('created_at', 'desc');
-                break;
-        }
-
-        $inquiries = $query->where(function ($query) use ($request) {
-            if ($searchStatus = $request->input('search_status')) {
-                $statusValue = null;
-
-                switch ($searchStatus) {
-                    case '未対応':
-                        $statusValue = 'default';
-                        break;
-                    case '対応中':
-                        $statusValue = 'checking';
-                        break;
-                    case '対応済み':
-                        $statusValue = 'checked';
-                        break;
-                    case 'default':
-                    case 'checking':
-                    case 'checked':
-                        $statusValue = $searchStatus;
-                        break;
-                }
-
-                if ($statusValue !== null) {
-                    $query->where('status', $statusValue);
-                }
-            }
-
-            if ($searchCompany = $request->input('search_company')) {
-                $query->where('company', 'LIKE', "%{$searchCompany}%");
-            }
-
-            if ($searchTel = $request->input('search_tel')) {
-                $query->where('tel', 'LIKE', "%{$searchTel}%");
-            }
-        })->paginate(20);
-
-        foreach ($inquiries as $inquiry) {
-            $inquiry->status = config('const.status')[$inquiry->status] ?? $inquiry->status;
-        }
-
-        return view('account.InquiryList', compact('inquiries', 'sort'));
+        return view('account.InquiryList', compact('inquiries'));
     }
 
     public function edit(Post $inquiry)
--- app/Services/InquiryService.php
+++ app/Services/InquiryService.php
@@ -6,6 +6,67 @@ use App\Models\Post;
 
 class InquiryService
 {
+
+    public function index($request)
+    {
+        $sort = $request->input('sort', 'newest');
+
+        $query = Post::query();
+
+        switch ($sort) {
+            case 'newest':
+                $query->orderBy('created_at', 'desc');
+                break;
+            case 'oldest':
+                $query->orderBy('created_at', 'asc');
+                break;
+            default:
+                $query->orderBy('created_at', 'desc');
+                break;
+        }
+
+        $inquiries = $query->where(function ($query) use ($request) {
+            if ($searchStatus = $request->input('search_status')) {
+                $statusValue = null;
+
+                switch ($searchStatus) {
+                    case '未対応':
+                        $statusValue = 'default';
+                        break;
+                    case '対応中':
+                        $statusValue = 'checking';
+                        break;
+                    case '対応済み':
+                        $statusValue = 'checked';
+                        break;
+                    case 'default':
+                    case 'checking':
+                    case 'checked':
+                        $statusValue = $searchStatus;
+                        break;
+                }
+
+                if ($statusValue !== null) {
+                    $query->where('status', $statusValue);
+                }
+            }
+
+            if ($searchCompany = $request->input('search_company')) {
+                $query->where('company', 'LIKE', "%{$searchCompany}%");
+            }
+
+            if ($searchTel = $request->input('search_tel')) {
+                $query->where('tel', 'LIKE', "%{$searchTel}%");
+            }
+        })->paginate(20);
+
+        foreach ($inquiries as $inquiry) {
+            $inquiry->status = config('const.status')[$inquiry->status] ?? $inquiry->status;
+        }
+
+        return $inquiries;
+    }
+    
     public function unresolvedInquiryCount()
     {
         return Post::where('status', 'default')->count();
